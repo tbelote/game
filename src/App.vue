5a31c89b13e4a09ef9bf3dc669ad7505c78b0eb2
Tom Belote <tom@shelfset.app>
2022-04-03 13:48:32 -0400

    saves library

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
- <h1>You've gotten a card.</h1>
+    <button id="showLibary" name="showLibrary" value="Library" v-on:click="showLibrary">Library</button> <button id="drawCard" name="drawCard" value="Draw Card" v-on:click="drawCard">Draw Card</button>
+  <div id="library">
+    <template v-for="item in myLibrary">
+      <div :key="item">
+        <img :src="item" />
+        <div><b>Name:</b>{{ item.replace(/.*\/img\//i,'').replace('.jpeg','').split('-')[0] }}</div>
+        <div><b>Rarity:</b>{{ item.replace(/.*\/img\//i,'').replace('.jpeg','').split('-')[1].split('.')[0] }}</div>
+      </div>
+    </template>
+  </div>
+  <div id="scratch">
+  <h1>You've gotten a card.</h1>
   <h2>Scratch to reveal your card.</h2>
   <vue-scratchable
     v-slot="{ init }"
@@ -11,13 +22,15 @@
   >
     <div class="wrapper">
       <img
+        id="currentCard"
         :src="character"
         @load="init()"
       >
       <h3>{{ subline }}</h3>
     </div>
   </vue-scratchable>
-  <p>You scratched {{ percentage }}% free.</p>
+  <p>You scratched off {{ percentage }}%.</p>
+  </div>
 </div>
 </template>
 
@@ -32,12 +45,30 @@ export default {
   computed: {
     subline() {
       return this.percentage < 60
-        ? `🎉 There is still ${100 - this.percentage}% left for me to be free... 🎉`
+        ? `🎉 There is still ${100 - this.percentage}% left for me to be scratched off... 🎉`
         : `💚 Hello, I'm named ${this.name} and I'm ${this.rarity}. 💚`;
     },
   },
+  watch: {
+    subline: {
+      handler: function() {
+        var complete = this.percentage >= 60;
+        if (complete) {
+          var url = document.getElementById("currentCard").src;
+          if (!this.myLibrary.includes(url)) {
+            this.myLibrary.push(url);
+            console.log(this.myLibrary);
+            localStorage.setItem("myLibrary", JSON.stringify(this.myLibrary));
+          }
+        }
+      }
+    }
+  },
   data() {
-    
+    var myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+    if (!myLibrary) {
+      myLibrary = [];
+    }
     var characters = ['amy-rare',
     'bow-common',
     'boxy-rare',
@@ -52,6 +83,8 @@ export default {
     
     return {
       percentage: 0,
+      characters: characters,
+      myLibrary: myLibrary,
       selectedCharacter: characters[selectedCharacter],
       name:name,
       rarity:rarity,
@@ -71,6 +104,25 @@ export default {
     updatePoints(percentage) {
       this.percentage = percentage;
     },
+    showLibrary() {
+      document.getElementById("showLibary").style.display = "none";
+      document.getElementById("drawCard").style.display = "block";
+      document.getElementById("library").style.display = "block";
+      document.getElementById("scratch").style.display = "none";
+    },
+    drawCard() {
+      var selectedCharacter = Math.floor(Math.random() * this.characters.length);
+      var name = this.characters[selectedCharacter].split('-')[0];
+      var rarity = this.characters[selectedCharacter].split('-')[1];
+      this.selectedCharacter = this.characters[selectedCharacter];
+      this.character = require('./assets/'+this.characters[selectedCharacter]+'.jpeg')
+      this.name = name;
+      this.rarity = rarity;
+      document.getElementById("showLibary").style.display = "block";
+      document.getElementById("drawCard").style.display = "none";
+      document.getElementById("library").style.display = "none";
+      document.getElementById("scratch").style.display = "block";      
+    }
   },
 }
 </script>
@@ -90,9 +142,15 @@ export default {
   margin-top: 50px;
 }
 
+#drawCard, #library {
+  display: none;
+}
+
 .vue-scratchable-wrapper {
   background-color: white;
 }
+h1, h2 { margin-top: 0;}
+
 
 h3 {
   color: #2c3e50;
